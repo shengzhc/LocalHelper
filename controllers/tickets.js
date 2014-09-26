73da512a650db6ddb4193905ce7d0adeea0b8ccf
Martin S <martinsong2608@gmail.com>
2014-09-26 16:23:09 -0700

    Add controller functions v1

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,7 +1,84 @@
-exports.createTicket = function(req, res, next) {
+var ticketModel = require('../models/ticketModel.js');
 
+/**
+ * Post Ticket create
+ *
+ * @param req
+ * @param res
+ * @param next
+ */
+exports.create = function(req, res, next) {
+	var ticket = new ticketModel({
+		summary: req.body.summary,
+		status: 'open',
+		category: req.body.category,
+		createdBy: req.body.createdBy
+	});
+	ticket.save(function (err, ticket) {
+		if (err) {
+			next(err);
+		}
+		res.write(JSON.stringify({
+			ticketId: ticket.id
+		}));
+	});
 };
 
-exports.getTickets = function(req, res, next) {
+/**
+ * Get Ticket list based on query strings
+ *
+ * @param req
+ * @param res
+ * @param next
+ */
+exports.list = function(req, res, next) {
+	var query = req.body;
+	ticketModel.find(query, function(err, tickets) {
+		if (err) next(err);
+		if (tickets) {
+			res.write(JSON.stringify(tickets));
+		}
+	});
+};
 
+/**
+ * Modify ticket 
+ *
+ * @param req
+ * @param res
+ * @param next
+ */
+exports.edit = function(req, res, next) {
+	var conditions = {
+		ticketId: req.body.ticketId
+	};
+	var updates = {
+		summary: req.body.summary,
+		status: req.body.status,
+		category: req.body.category,
+		updatedTime: Date()
+	};
+	ticketModel.update(conditions, updates, function(err, tickets) {
+		if (err) next(err);
+		res.write(JSON.stringify(tickets));
+	});	
+};
+
+/**
+ * Delete all the tickets based on criteria
+ * @param req
+ * @param res
+ * @param next
+ */
+exports.destroy = function(req, res, next) {
+	var conditions = {
+		ticketId: req.body.ticketId
+	};
+	ticketModel.find(conditions, function(err, ticket) {
+		if (err) next(err);
+		ticketModel.remove(function(err, ticket) {
+			if (err) next(err);
+			res.write(JSON.stringify({result: 'success'}));
+		});
+	});
 };
--- controllers/users.js
+++ controllers/users.js
@@ -1,15 +1,80 @@
+var userModel = require('../models/userModel.js');
+
+/**
+ * Post user create
+ * @param req
+ * @param res
+ * @param next
+ */
 exports.create = function(req, res, next) {
-
+    var user = new userModel({
+        username: req.body.username,
+        password: req.body.password
+    });
+    user.save(function (err, user) {
+        if (err)    next(err);
+        res.write(JSON.stringify({
+            userId: user.id
+        }));
+    });
 };
 
-exports.get = function(req, res, next) {
-
+/**
+ * Get user list
+ * @param req
+ * @param res
+ * @param next
+ */
+exports.list = function(req, res, next) {
+    var query = {};
+    if (req.body.name) {
+        query.name = req.body.name;
+    }
+    userModel.find(query, function(err, users) {
+        if (err) next(err);
+        if (users) {
+            res.write(JSON.stringify(users));
+        }
+    });
 };
 
-exports.update = function(req, res, next) {
-
+/**
+ * Update user edit
+ * @param req
+ * @param res
+ * @param next
+ */
+exports.edit = function(req, res, next) {
+    var conditions = {
+        username: req.body.username
+    };
+    var updates = {
+        name: req.body.name,
+        address: req.body.address,
+        phone: req.body.phone
+    };
+    userModel.update(conditions, updates, function(err, user) {
+        if (err) next(err);
+        res.write(JSON.stringify(user));
+    });
 };
 
-exports.delete = function(req, res, next) {
-
+/**
+ * Delete user remove
+ * @param req
+ * @param res
+ * @param next
+ */
+exports.destroy = function(req, res, next) {
+    var conditions = {
+        username: req.body.username
+    };
+    userModel.find(conditions, function(err, user) {
+        if (err) next(err);
+        userModel.remove(function(err, user) {
+            if (err) next(err);
+            res.write(JSON.stringify({result: 'success'}));
+        });
+    });
 };
+
