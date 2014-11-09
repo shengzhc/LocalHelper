0b8277c06cb9e5bed8c688d564e06f97e96232f7
Shengzhe Chen <shengzhe.chen@gmail.com>
2014-11-09 11:02:23 -0800

    finish session_manager test

---
+++ tests/controllers/input_validator_test.js
@@ -0,0 +1,23 @@
+
+describe('Input_Validator', function() {
+
+		describe('#user_signup()', function() {
+			
+		});
+
+		describe('#user_signin()', function() {
+
+		});
+
+		describe('#user_signout()', function() {
+
+		});
+
+		describe('#user_update()', function() {
+
+		});
+
+		describe('#user_deactivate()', function() {
+
+		});
+});
--- tests/controllers/session_manager_test.js
+++ tests/controllers/session_manager_test.js
@@ -35,21 +35,42 @@ describe('Session_Manager', function() {
 			it('should be typeof function', function() {
 				session_manager.deactivateSession.should.be.a.Function;
 			});
+
 			it('should clear request session and remove from session mananger', function(done) {
-				var token = 'test';
-				session_manager.secureTokens.push(token);
-				session_manager.deactivateSession(req, function(err) {
-					assert.equal(req.session.hasOwnProperty('token'), false);
-					assert.equal(req.session.hasOwnProperty('others'), false);
-					assert.equal(session_manager.secureTokens.indexOf(token), -1);
-					done(err);
-				});
+				var app = express()
+					.use(session({secret: '_local_helper', resave:true, saveUninitialized:true}))
+					.use(function(req, res, next) {
+						session_manager.activateSession(req, function() {
+							var token = req.session.token;
+							session_manager.deactivateSession(req, function(err) {
+								req.should.not.have.property('session');
+								session_manager.secureTokens.should.not.containEql(token);
+								next(err);
+							});
+						});
+					})
+					.use(end);
+				request(app).get('/').end(function(err, res) {done(err)});
 			});
 		});
 
 		describe('#verifySession()', function() {
 			it('should be typeof function', function() {
 				session_manager.verifySession.should.be.a.Function;
+			});
+
+			it('should pass session verify', function(done) {
+				var app = express()
+					.use(session({secret: '_local_helper', resave:true, saveUninitialized:true}))
+					.use(function(req, res, next) {
+						session_manager.activateSession(req, function() {
+							session_manager.verifySession(req, function(err, isValid) {
+								isValid.should.be.true;
+								next(err);
+							});
+						});
+					});
+				request(app).get('/').end(done);
 			});
 		});
 });
